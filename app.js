26b860d1193d9f2f69d2a84cc4f1cab71df6fa2c
Alejandro Figar Gutierrez <afigar@gmail.com>
2019-04-18 21:32:46 -0300

    fourth commit

--- app.js
+++ app.js
@@ -1,30 +1,3 @@
-const driver = require('bigchaindb-driver')
-const express = require('express')
-const app = express()
-
-const alice = new driver.Ed25519Keypair()
-const conn = new driver.Connection('https://test.bigchaindb.com/api/v1/')
-const tx = driver.Transaction.makeCreateTransaction(
-    { message: 'Hola Mundo AF' },
-    null,
-    [ driver.Transaction.makeOutput(
-        driver.Transaction.makeEd25519Condition(alice.publicKey))],
-    alice.publicKey)
-const txSigned = driver.Transaction.signTransaction(tx, alice.privateKey)
-conn.postTransactionCommit(txSigned)
-
-app.get('/', (req, res) => {
-  res.status(200).send('Hello, world!').end();
-});
-
-// Start the server
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-  console.log('Press Ctrl+C to quit.');
-});
-
-
 var express    = require('express')
 var bodyParser = require('body-parser')
 const driver = require('bigchaindb-driver')
@@ -47,8 +20,13 @@ app.use(function (req, res, next) {
         driver.Transaction.makeEd25519Condition(alice.publicKey))],
     alice.publicKey)
   const txSigned = driver.Transaction.signTransaction(tx, alice.privateKey)
-  conn.postTransactionCommit(txSigned)
+  console.log(conn.postTransactionCommit(txSigned))
   next()
 })
 
-app.listen(3000);
+// Start the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
+});
